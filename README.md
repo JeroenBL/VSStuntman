ec01fcb8a26aa93f86aa62be2f05f7b3295a374f
jblaauw <j.blaauw@tools4ever.com>
2021-04-27 00:13:06 +0200

    initial release

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # VSStuntman
+
+![logo](https://raw.githubusercontent.com/Jeroenbl/Stuntman/main/logo.png)
+
+## Because every app needs a stuntman!
+
+VSStuntman is an add-on for the PSStuntman PowerShell module. It allows you to import the saved stuntman from the PSStuntman database, directly into VSCode as a PowerShell code snippet. 
+
+The PowerShell snippet is formatted as a HelloID person object. This makes it a little easier to develop HelloID connectors within VSCode.
+
+## Features
+
+- Importing stuntman created with the PSStuntman PowerShell module.
+
+![import](https://raw.githubusercontent.com/jeroenbl/Stuntman/main/assets/importStuntman.gif)
+
+## Requirements
+
+- Windows PowerShell 5.1
+- .NET Framework 4.8
+- PSStuntman PowerShell module https://github.com/jeroenbl/PSStuntman
+
+## Extension Settings
+
+This extension adds the following settings:
+
+This extension contributes the following settings:
+
+* `Stuntman.DatabaseFileLocation`: The location to the database (sqlite) on disk.
+
+![Configure](https://raw.githubusercontent.com/jeroenbl/Stuntman/main/assets/stuntmanConfiguration.png)
+
+## Known Issues
+
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of Stuntman
+
+## Contributing
+
+Find a bug or have an idea! Open an issue or submit a pull request!
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,68 @@
+import * as vscode from 'vscode';
+import * as sqlite3 from 'sqlite3';
+import { Console } from 'console';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+		
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('stuntman.ImportStuntman', () => {
+
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+		  vscode.window.showInformationMessage("editor does not exist");
+		  return;
+		}
+
+		var dbLoc = vscode.workspace.getConfiguration('stuntman').databaseFileLocation;		
+
+		let db = new sqlite3.Database(dbLoc, (err) => {
+			if (err) {
+			  vscode.window.showErrorMessage(err.message);
+			  return;
+			}
+			vscode.window.showInformationMessage("Connected to the 'Stuntman' SQlite database");
+		  }
+		);
+	
+		var stuntman = Array();
+		db.all('SELECT * FROM stuntman', [], (err, rows) => {
+			if (err) {
+			  throw err;
+			}
+			rows.forEach((row) => {
+				stuntman.push(row);
+			});
+			const quickPick = vscode.window.createQuickPick();
+			quickPick.items = stuntman.map((x: any) => ({ label: x.DisplayName }));
+			quickPick.onDidChangeSelection(([item]) => {
+				const selection = editor.selection;
+				if (item) {
+					db.get(`SELECT * FROM Stuntman WHERE DisplayName = "${ item.label }"`, (err, row) => {
+						if (err) {
+						  return vscode.window.showErrorMessage(err.message);
+						}
+						return row
+						  ? editor.edit(edit => {
+								const str = '$json = @"' + '\n' + JSON.stringify(row) + '\n' + '"@' + '\n' + "$person = $json | ConvertFrom-Json"; 
+								edit.replace(selection, str);
+						  })
+						  : vscode.window.showErrorMessage(`No stuntman found with DisplayName ${ item.label }`);
+					  });
+					}
+					quickPick.dispose();
+				});
+			  quickPick.onDidHide(() => quickPick.dispose());
+			  quickPick.show();
+		});
+	});
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
